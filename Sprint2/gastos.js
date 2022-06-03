dc2be11e7523f4a157e6871b85f922114968d926
mfolino <matefolino@gmail.com>
2022-06-03 18:18:27 -0300

    APP

---
+++ Sprint2/gastos.js
@@ -0,0 +1,11 @@
+var nombre = []
+let monto = []
+var cont = 0
+
+function tot(){
+    var nom = document.getElementById('ing_nom').value
+    nombre = nom;
+    console.log(nombre);
+    cont++
+    console.log(cont)
+}
